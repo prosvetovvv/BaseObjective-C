a7b9321c734ab7948f3272c83a7c5cb6493bc4cb
prosvetovvv <vp12@yandex.ru>
2021-01-27 11:26:41 +0300

    Initial Commit

---
+++ BaseObjective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BaseObjective-C
+//
+//  Created by Vitaly Prosvetov on 27.01.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
